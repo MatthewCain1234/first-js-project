076be8327bfafb7c86cd6de00ebf7c5f6bc7f439
MatthewCain1234 <m.cain12834@gmail.com>
2020-11-06 19:35:36 -0500

    array fixes
    If applied, this will fix the array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
 let pokemonList = [
   Bulbasaur = {
-    height: 0.7
-    weight: 6.9
+    height: 0.7,
+    weight: 6.9,
     type: ["grass", "poison"]
-  }
+  },
   Charmander = {
-    height: 0.6
-    weight: 8.9
+    height: 0.6,
+    weight: 8.9,
     type: ["fire"]
-  }
+  },
   Squirtle = {
-    height: 0.5
-    weight: 9
+    height: 0.5,
+    weight: 9,
     type: ["water"]
   }
 ]
