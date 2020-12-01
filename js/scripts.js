82fe66a6bd3116cc40dbff22d5e9d8a50cbd8f24
Matthew Cain <m.cain12834@gmail.com>
2020-12-01 13:45:14 -0500

    DOM fixes
    If applied, this commit will fix errors relating to the showDetails(pokemon) function.

--- js/scripts.js
+++ js/scripts.js
@@ -33,13 +33,15 @@ let pokemonRepository = (function () {
     button.classList.add("button-class");
     listpokemon.appendChild(button);
     pokemonList.appendChild(listpokemon);
-    button.addEventListener('click', function (pokemon) {
-      showDetails(pokemon)
-    })
-  };
+
+    function details(pokemon) {
+      return showDetails(pokemon);
+    }
+    button.addEventListener('click', () => details(pokemon))
+  }
 
   function showDetails(pokemon) {
-    console.log(pokemon)
+    console.log('pokemon name:', pokemon.name)
   }
 
   return {
